db3d487fa95141e0ac9bfe6f1a8d08186fc43505
Murtaza Gulamali <mygulamali@gmail.com>
2016-01-10 21:06:39 +0000

    Reorganise tests

---
+++ tests/julian_day_tests.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "julian_day.h"
+
+void get_calendar_type_test(void **state);
+void julian_day_test(void **state);
+void get_year_type_test(void **state);
+void modified_julian_day_test(void **state);
